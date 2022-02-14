63042437ec0dc154865ea3168322115e3547efee
Trey Tabner <trey@tabner.com>
2022-02-14 11:19:24 -0600

    Fix readability

--- stale/__init__.py
+++ stale/__init__.py
@@ -6,6 +6,7 @@ abandoned issues after a period of inactivity
 import argparse
 import logging
 import os
+import sys
 
 from datetime import datetime, timedelta
 
@@ -21,9 +22,9 @@ had recent activity. It will be closed if no further activity occurs.
 
 
 LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
-logging.basicConfig(format=LOG_FORMAT, datefmt='%c')
+logging.basicConfig(format=LOG_FORMAT, datefmt='%c', stream=sys.stdout)
 logger = logging.getLogger("__name__")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 def get_args():
@@ -35,10 +36,8 @@ def get_args():
         description='Handle stale Github issues')
 
     parser.add_argument('repo', help="Repo to manage")
-    parser.add_argument('--base-url', default='api.github.com',
-                        help="Github API URL, defaults to api.github.com")
-    parser.add_argument('--dry-run', action='store_const', const=True,
-                        help="Just show what would happen")
+    parser.add_argument('--base-url', default='api.github.com', help="Github API URL, defaults to api.github.com")
+    parser.add_argument('--dry-run', action='store_const', const=True, help="Just show what would happen")
 
     return parser.parse_args()
 
@@ -54,11 +53,11 @@ class Stale():
         """
         self.args = get_args()
         logger.info("Connecting to %s", self.args.base_url.split('/')[0])
-        self.github = Github(base_url='https://' + self.args.base_url,
-                             login_or_token=os.environ.get("GITHUB_TOKEN"))
+        self.github = Github(base_url='https://' + self.args.base_url, login_or_token=os.environ.get("GITHUB_TOKEN"))
         logger.info("Using repo %s", self.args.repo)
         self.repo = self.github.get_repo(self.args.repo)
         self.config = self.get_config()
+        self.processed = 0
 
     def get_config(self):
         """
@@ -78,11 +77,11 @@ class Stale():
             return True
 
         if issue.assignees and self.config.get('exemptAssignees', True):
-            logger.info("Issue %d has assignees, skipping", issue.number)
+            logger.info("Issue %s has assignees, skipping", issue.html_url)
             return True
 
         if issue.milestone and self.config.get('exemptMilestones', True):
-            logger.info("Issue %d is in a milestone, skipping", issue.number)
+            logger.info("Issue %s is in a milestone, skipping", issue.html_url)
             return True
 
         return False
@@ -91,21 +90,17 @@ class Stale():
         """
         Fetch eligible issues and start processing
         """
-        processed = 0
-        issues = self.repo.get_issues(state='open', sort='updated-asc',
-                                      labels=self.config.get('onlyLabels', []))
+        issues = self.repo.get_issues(state='open', sort='updated-asc', labels=self.config.get('onlyLabels', []))
         for issue in issues:
-            if (self.config.get('limitPerRun') and
-                    processed >= self.config.get('limitPerRun')):
-                logger.warning("Processed %d issues already, ending.",
-                               processed)
+            if (self.config.get('limitPerRun') and self.processed >= self.config.get('limitPerRun')):
+                logger.info("Processed enough issues already, ending for now.")
                 return
 
             if self.is_exempt(issue):
                 continue
 
             if self.process_issue(issue):
-                processed += 1
+                self.processed += 1
 
     def stale_path(self, issue):
         """
@@ -115,43 +110,31 @@ class Stale():
         until_close = self.config.get('daysUntilClose')
         if until_close:
             close_date = datetime.utcnow() - timedelta(days=until_close)
-            logger.debug("Close stale if older than: %s",
-                         close_date.isoformat())
+            logger.debug("Close stale if older than: %s", close_date.isoformat())
         else:
             return False
 
         last_comment = issue.get_comments().reversed[0]
-        if last_comment.body == self.config.get('markComment',
-                                                DEFAULT_MARK_COMMENT):
+        if last_comment.body == self.config.get('markComment', DEFAULT_MARK_COMMENT):
             # check date and possibly close
             if last_comment.updated_at < close_date:
-                if self.config.get('closeComment'):
-                    logger.info("Adding close comment to issue %d",
-                                issue.number)
-                    if not self.args.dry_run:
-                        issue.create_comment(
-                            self.config.get('closeComment'))
-                logger.info("Closing issue %d", issue.number)
-                if self.config.get('closeComment'):
-                    issue.create_comment(self.config.get('closeComment'))
                 if not self.args.dry_run:
+                    if self.config.get('closeComment'):
+                        logger.info("Adding close comment to issue %d", issue.number)
+                        issue.create_comment(self.config.get('closeComment'))
+                    logger.info("Closing issue %d", issue.number)
                     issue.edit(state='closed')
                 return True
 
             logger.info("Issue %d is not stale enough", issue.number)
-            logger.debug("Last comment at: %s",
-                         last_comment.updated_at.isoformat())
-            logger.debug("Would be able to close in %s hours",
-                         issue.updated_at - close_date)
+            logger.debug("Last comment at: %s", last_comment.updated_at.isoformat())
+            logger.debug("Would be able to close in %s hours", issue.updated_at - close_date)
 
         else:
-            logger.info("New comment found, on issue %d, "
-                        "removing stale label", issue.number)
+            logger.info("New comment found, on issue %d, " "removing stale label", issue.number)
             if not self.args.dry_run:
-                logger.info("Removing stale label from issue %d",
-                            issue.number)
-                issue.remove_from_labels(
-                    self.config.get('staleLabel', 'stale'))
+                logger.info("Removing stale label from issue %d", issue.number)
+                issue.remove_from_labels(self.config.get('staleLabel', 'stale'))
                 if self.config.get('unmarkComment'):
                     issue.create_comment(self.config.get('unmarkComment'))
             return True
@@ -162,7 +145,7 @@ class Stale():
         """
         Process a specific issue by checking for activity and staleness
         """
-        logger.info("Processing %s (%s)", issue.number, issue.title)
+        logger.info("Processing %s (%s)", issue.html_url, issue.title)
         logger.debug("Current time: %s", datetime.utcnow().isoformat())
 
         until_stale = self.config.get('daysUntilStale', 60)
@@ -174,17 +157,15 @@ class Stale():
             return self.stale_path(issue)
 
         logger.debug("Last update at %s", issue.updated_at.isoformat())
-        logger.debug("Would be stale in %s hours",
-                     issue.updated_at - stale_date)
+        logger.debug("Would be stale in %s hours", issue.updated_at - stale_date)
         if issue.updated_at < stale_date:
             logger.info("Marking issue %d stale", issue.number)
             if not self.args.dry_run:
                 issue.add_to_labels(self.config.get('staleLabel', 'stale'))
-                issue.create_comment(self.config.get('markComment',
-                                     DEFAULT_MARK_COMMENT))
+                issue.create_comment(self.config.get('markComment', DEFAULT_MARK_COMMENT))
             return True
 
-        logger.info("Issue %d has recent activity, skipping", issue.number)
+        self.processed = self.config.get('limitPerRun')
         return False
 
 
